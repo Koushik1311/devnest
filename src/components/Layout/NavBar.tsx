ad334a0599cebcb7c684d362eece30f7cded42b6
Koushik <programmer.koushik@gmail.com>
2024-03-02 22:36:42 +0530

    landing page

---
+++ src/components/Layout/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+  return (
+    <section className="mt-[1rem] flex items-center justify-between">
+      <h1>DevNest</h1>
+      <ul className="flex">
+        <li>Home</li>
+        <li>Cources</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </section>
+  );
+}
